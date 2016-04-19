0970404e7b22b659cd7f539d84c10fba129fe484
RhenarSBR <contact@studioblackraven.fr>
2016-04-19 16:56:53 +0200

    Version 0.3
    Compactage en un seul fichier à mettre dans le dossier
    darkrpmodification puis module

--- lua/base/sh_stamina.lua
+++ lua/base/sh_stamina.lua
@@ -2,19 +2,21 @@
 -- Stamina Reloaded --
 ----------------------
 -- Auteur : Rhenar ---
--- Version : 0.1 -----
+-- Version : 0.3 -----
 ----------------------
-AddCSLuaFile()
+if (SERVER) then
 
-include("lua/base/sv_spawn.lua")
-include("lua/base/sv_stopsprint.lua")
+function createstamina()
+	
+	timer.Destroy("staminatimer")
+	ply:SetRunSpeed(240)
+	ply:SetNWInt("stamina",100)
 
--- ConVars --
+	staminarestore(ply)
 
-ply = LocalPlayer()
+end
 
------------------------------
-
+hook.Add("spawnply","createstamina", createstamina)
 function stamina(ply, key)
 	if key == IN_SPEED or ply:KeyDown(IN_SPEED) then
 		if ply:InVehicle() then return false end
@@ -42,7 +44,7 @@ function stamina(ply, key)
 	if key == IN_JUMP or ply:KeyDown(IN_JUMP) then
 		if ply:GetNWInt("stamina") >= 10 then
 			ply:SetJumpPower(200)
-			ply:SetNWInt("stamina",ply:GetNWInt("stamina",) - 1 )
+			ply:SetNWInt("stamina",ply:GetNWInt("stamina") - 1 )
 		else
 			ply:SetJumpPower(20)
 		end
@@ -50,3 +52,23 @@ function stamina(ply, key)
 end
 
 hook.Add("press", "stamina", stamina)
+
+function staminaarret(ply, key)
+	if key == IN_SPEED and !ply:KeyDown(IN_SPEED) then
+		timer.Destroy("staminatimer")
+		staminarestore(ply)
+	end
+end
+
+hook.Add("arret", "staminaarret", staminaaret)
+
+function staminarestore(ply)
+	timer.Create("staminagain", 0.50, 0, function()
+		if ply:GetNWInt("stamina") >= 100 then
+			return false
+		else
+			ply:SetNWInt("stamina",ply:GetNWInt("stamina") + 1)
+		end
+	end)
+end
+end
